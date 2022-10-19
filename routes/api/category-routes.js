dbbd5ca596cef1993726f6af042b3af213765532
AndrewKamSki <104537278+AndrewKamSki@users.noreply.github.com>
2022-10-19 07:09:15 -0400

    Added route to get category by its id/PK

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,6 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
       include: [
@@ -22,9 +21,22 @@ router.get('/', async (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        },
+      ],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
